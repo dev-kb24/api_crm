e51813d1b1bfe56eecf2d8550af89dbeeae66ccf
rémi <dufour.remi59@gmail.com>
2023-09-06 18:47:39 +0200

    init add auth (user)

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,34 +1,40 @@
-import { Controller, Get, Post, Put, Delete, ConflictException } from '@nestjs/common';
-import { Body, Param } from '@nestjs/common/decorators'
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import { Body, Param, UseGuards } from '@nestjs/common/decorators'
 import { InputProduct } from './dto/inputProduct';
 import { ProductsService } from './products.service';
 import { OutputProduct } from './dto/outputProduct';
 import { InputProductId } from './dto/inputProductId';
+import { UsersGuard } from 'src/users/users.guard';
 
 @Controller('products')
 export class ProductsController {
     constructor(private productsService : ProductsService){}
 
+    @UseGuards(UsersGuard)
     @Post()
     async create(@Body() inputProduct : InputProduct) : Promise<OutputProduct>{
         return await this.productsService.create(inputProduct);
     }
 
+    @UseGuards(UsersGuard)
     @Put(":productId")
     async update(@Body() inputProduct : InputProduct, @Param() inputProductId: InputProductId) : Promise<OutputProduct> {
         return this.productsService.update(inputProduct,inputProductId);
     }
-    
+
+    @UseGuards(UsersGuard)
     @Delete(":productId")
     async delete(@Param() inputProductId : InputProductId) : Promise<OutputProduct>{
         return this.productsService.delete(inputProductId);
     }
 
+    @UseGuards(UsersGuard)
     @Get()
     async findAll() : Promise<OutputProduct[]>{
         return this.productsService.findAll();
     }
 
+    @UseGuards(UsersGuard)
     @Get(":productId")
     async findById(@Param() inputProductId : InputProductId) : Promise<OutputProduct>{
         return this.productsService.findById(inputProductId);
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersController } from './users.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    ConfigModule, 
+    JwtModule.registerAsync({
+      imports: [ConfigModule], 
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '3600s' },
+      }),
+      inject: [ConfigService], 
+    }),
+  ],
+  providers: [UsersService],
   controllers: [UsersController],
-  providers: [UsersService]
+  exports: [UsersService],
 })
 export class UsersModule {}
